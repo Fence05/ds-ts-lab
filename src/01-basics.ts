b181bb4e1ef3c44338022036e62664b32fc21231
Aurimas <arimkus04@gmail.com>
2026-09-14 22:52:01 +0100

    Typed function half-signature.

--- src/01-basics.ts
+++ src/01-basics.ts
@@ -1,22 +1,5 @@
-interface Friend {
-    name: string;
-    phone: string;
-    age: number
-}
+import {Friend, Colleague, ColleagueHistory } from './myTypes'
 
-interface Colleague {
-    name: string;
-    department: string;
-    contact: {
-        email: string;
-        extension: number;
-    };
-}
-
-interface ColleagueHistory {
-  current: Colleague[],
-  former: Colleague[]
-}
 
 const friend1 = {
   name: "Paul Fleming",
@@ -30,8 +13,8 @@ const friend2 = {
   age: 31,
 };
 
-const friends = [friend1, friend2];
-console.log(friends[1]);
+export const friends = [friend1, friend2];
+//console.log(friends[1]);
 
 //   -------------------
 const colleague1 = {
@@ -61,9 +44,9 @@ const colleague3 = {
   },
 };
 
-export const colleagues : ColleagueHistory = {
+export const colleagues = {
   current: [colleague1, colleague2, colleague3],
   former: [],
 };
 
-console.log(colleagues.current[0]);
+//console.log(colleagues.current[0]);
