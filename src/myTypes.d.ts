c3b3ccd57a8450144f7c2c3fe1502326cdfd4d63
BlackAmber <arimkus04@gmail.com>
2026-09-24 11:07:23 +0100

    Define types using Utility types.

--- src/myTypes.d.ts
+++ src/myTypes.d.ts
@@ -48,3 +48,8 @@ export type BuddyList = {
 
 
 
+export type FriendPartial = Partial<Friend>
+// Type for gaining access to an event, e.g. concert.
+export type EventPass = Omit<Colleague, "contact"> & {passCode : number;}
+// Immutable person type, based on Friend type.
+export type SecureFriendContact = Readonly<Pick<Friend,"name" | "phone" > >
